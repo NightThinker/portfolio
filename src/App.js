b7b599beca6f724558f3a5f8abf5e030ea7470d5
NightThinker <travel_special@hotmail.com>
2019-08-24 21:49:33 +0700

    implement canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import { usePersistentCanvas } from './utils/canvas';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Thonburi;
+    -webkit-font-smoothing: antialiased;
+    font-size: 16px;
+  }
+  h1 {
+    font-size: 1.25rem;
+    margin: 0;
+  }
+  h2 {
+    font-size: 1.125rem;
+    margin: 0;
+  }
+  h3 {
+    font-size: 1rem;
+    margin: 0;
+  }
+  p {
+    font-size: 0.875rem;
+    margin: 0;
+  } 
+`;
 
 function App() {
+  const [canvasRef] = usePersistentCanvas();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <GlobalStyle />
+      <h1>test</h1>
+      <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} />
+    </React.Fragment>
   );
 }
 
